9bbb24e40c141c88df3bb2fafad8f3d9750dffb7
HueHueberry <huby.new1112@gmail.com>
2022-09-03 23:43:42 +0800

    Encryption and Decryption defined WARNING: weirdCryptor encryption and decryption are not yet tested DO NOT implement these yet!!!

--- app/src/main/java/encryption/WeirdCryptor.kt
+++ app/src/main/java/encryption/WeirdCryptor.kt
@@ -1,13 +1,17 @@
 package encryption
 
 class WeirdCryptor {
-    var outputHolderEncrypted: CharArray = CharArray(0)
-    var outputHolderDecrypted: String = ""
-
     var inputText: String = ""
+    var inputCharArr: CharArray = CharArray(0)
     var keyText: String = ""
+
     constructor(plainText: String, keyText: String) {
         this.inputText = plainText
+        this.keyText = keyText
+    }
+
+    constructor(charArr: CharArray, keyText: String) {
+        this.inputCharArr = charArr
         this.keyText = keyText
     }
 
@@ -39,7 +43,48 @@ class WeirdCryptor {
         return (initialShift xor rightCut).toChar()
     }
 
+    // literally encrypts the buffer
+    fun encrypt(): CharArray {
+        val valueHolder: CharArray = CharArray(inputText.length)
+        inputText.forEachIndexed { index, character ->
+            var generatedKeyValue = this.getKeyVal()
+            val keyLength = keyText.length
 
-    fun encrypt() { }
-    fun decrypt() { }
+            // oops, another bias
+            if (generatedKeyValue % 2 == 0) {
+                generatedKeyValue += 1
+            }
+
+            val firstPart = shiftLeft8(character.code, generatedKeyValue).code
+            val secondPart = firstPart xor keyText[index % keyLength].code
+            val finalPart = shiftRight8(secondPart, generatedKeyValue)
+
+            valueHolder[index] = finalPart
+        }
+
+        return valueHolder
+    }
+
+    // decrypts the encrypted buffer
+    fun decrypt(): String {
+        val valueHolder: CharArray = CharArray(inputCharArr.size)
+        inputCharArr.forEachIndexed { index, character ->
+            var generatedKeyValue = this.getKeyVal()
+            val keyLength = keyText.length
+
+            // bias retrieval again!!
+            if (generatedKeyValue % 2 == 0) {
+                generatedKeyValue += 1
+            }
+
+            // unwrapping the circular shifts
+            val firstReversePart = shiftLeft8(character.code, generatedKeyValue).code
+            val secondReversePart = firstReversePart xor keyText[index % keyLength].code
+            val finalReversePart = shiftRight8(secondReversePart, generatedKeyValue)
+
+            valueHolder[index] = finalReversePart
+        }
+
+        return String(valueHolder)
+    }
 }
